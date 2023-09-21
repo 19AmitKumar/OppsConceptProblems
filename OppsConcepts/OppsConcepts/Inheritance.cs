60a221ef5c581bf89703c6f44a9371a4a809eff3
19AmitKumar <ak9978292@gmail.com>
2023-09-21 19:57:44 +0530

    [Amit] Added:Inheritance Example

---
+++ OppsConcepts/OppsConcepts/Inheritance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppsConcepts
+{
+    public class Inheritance
+    {
+        public int EmpId;
+        public string EmpName;
+        public int EmpAge;
+        public long EmpContactNo;
+    }
+    public class VisitingEmployee : Inheritance
+    {
+        public int Visitingsalary;
+        public int VisitingHour;
+
+    }
+    public class ParmanentEmployee :Inheritance
+    {
+        public int ParmanentSalary;
+        public int ParmanentHour;
+    }
+}
--- OppsConcepts/OppsConcepts/Program.cs
+++ OppsConcepts/OppsConcepts/Program.cs
@@ -3,21 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace OppsConcepts
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            ClassAndObject obj = new ClassAndObject();
-            obj.num1 = 20;
-            obj.num2 = 10;
-            obj.Add();
-            obj.Subtract();
-            obj.Multiply();
-            obj.Divide();
-            Console.ReadLine();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your choice: ");
+                int a=Convert.ToInt32(Console.ReadLine());
+                switch (a)
+                {
+                        case 0:
+                        flag = false;
+                        break;
+                        case 1:
+                        ClassAndObject obj = new ClassAndObject();
+                        obj.num1 = 20;
+                        obj.num2 = 10;
+                        obj.Add();
+                        obj.Subtract();
+                        obj.Multiply();
+                        obj.Divide();
+                        break;
+                        case 2:
+                        VisitingEmployee v = new VisitingEmployee();
+                        v.EmpId = 1;
+                        v.EmpName = "Amit";
+                        v.EmpAge = 30;
+                        v.EmpContactNo = 7070939796;
+                        v.Visitingsalary = 30000;
+                        v.VisitingHour = 50;
+                        Console.WriteLine("Visiting Employee id is: " + v.EmpId);
+                        Console.WriteLine("Visiting Employee name is: " + v.EmpName);
+                        Console.WriteLine("Visiting Emloyee Age is: " + v.EmpAge);
+                        Console.WriteLine("Visiting Employee Contact no is: " + v.EmpContactNo);
+                        Console.WriteLine("Visiting Employee salary is: " + v.Visitingsalary);
+                        Console.WriteLine("Visiting Employee Hour is: " + v.VisitingHour);
+                        Console.WriteLine("----------------------------------");
+                        ParmanentEmployee p = new ParmanentEmployee();
+                        p.EmpId = 2;
+                        p.EmpName = "Satyam";
+                        p.EmpAge = 31;
+                        p.EmpContactNo = 8210813591;
+                        p.ParmanentSalary = 40000;
+                        p.ParmanentHour = 55;
+                        Console.WriteLine("Parmanent Employee id is: " + p.EmpId);
+                        Console.WriteLine("Parmanent Employee name is: " + p.EmpName);
+                        Console.WriteLine("Parmanent Employee Age is: " + p.EmpAge);
+                        Console.WriteLine("Parmanent Employee Contact no is: " + p.EmpContactNo);
+                        Console.WriteLine("Parmanent Employee salary is: " + p.ParmanentSalary);
+                        Console.WriteLine("Parmanent Employee Hour is: " + p.ParmanentHour);
+                        break;
+
+
+
+
+                        Console.ReadLine();
+                }
+            }
         }
     }
 }
