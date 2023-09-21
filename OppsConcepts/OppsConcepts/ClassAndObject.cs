5daad0359ca7f7676d36c421b8647a0513630de1
19AmitKumar <ak9978292@gmail.com>
2023-09-21 18:44:14 +0530

    [Amit] Added:Class and Object Problem

---
+++ OppsConcepts/OppsConcepts/ClassAndObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppsConcepts
+{
+    public class ClassAndObject
+    {
+        public int num1;
+        public int num2;
+       
+        public void Add()
+        {
+            int sum=num1 + num2;
+            Console.WriteLine("Sum is: "+sum);
+        }
+        public void Subtract() 
+        {
+            int sub=num1 - num2;
+            Console.WriteLine("The subtraction is: "+sub);
+        }
+        public void Multiply()
+        {
+            int mul=num1 * num2;
+            Console.WriteLine("The Multiplication is: "+mul);
+        }
+        public void Divide()
+        {
+            int div=num1 / num2;
+            Console.WriteLine("The division is: " + div);
+        }
+        
+    }
+}
---
+++ OppsConcepts/OppsConcepts/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppsConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassAndObject obj = new ClassAndObject();
+            obj.num1 = 20;
+            obj.num2 = 10;
+            obj.Add();
+            obj.Subtract();
+            obj.Multiply();
+            obj.Divide();
+            Console.ReadLine();
+        }
+    }
+}
